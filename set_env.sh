31e27ce484202924824845c220710472b4ecbed5
GottAuf <gordiefx@gmail.com>
2022-09-22 06:42:00 +0300

    modified:   .gitignore 	modified:   set_env.sh 	deleted:    udagram-api/.dockerignore 	deleted:    udagram-api/.eslintrc.json 	deleted:    udagram-api/.gitignore 	deleted:    udagram-api/.npmrc 	deleted:    udagram-api/mock/xander0.jpg 	deleted:    udagram-api/mock/xander1.jpg 	deleted:    udagram-api/mock/xander2.jpg 	deleted:    udagram-api/package-lock.json 	deleted:    udagram-api/package.json 	deleted:    udagram-api/src/aws.ts 	deleted:    udagram-api/src/config/config.ts 	deleted:    udagram-api/src/controllers/v0/feed/models/FeedItem.ts 	deleted:    udagram-api/src/controllers/v0/feed/routes/feed.router.ts 	deleted:    udagram-api/src/controllers/v0/index.router.ts 	deleted:    udagram-api/src/controllers/v0/model.index.ts 	deleted:    udagram-api/src/controllers/v0/users/models/User.ts 	deleted:    udagram-api/src/controllers/v0/users/routes/auth.router.ts 	deleted:    udagram-api/src/controllers/v0/users/routes/user.router.ts 	deleted:    udagram-api/src/migrations/20190325-create-feed-item.js 	deleted:    udagram-api/src/migrations/20190328-create-user.js 	deleted:    udagram-api/src/sequelize.ts 	deleted:    udagram-api/src/server.ts 	deleted:    udagram-api/tsconfig.json 	deleted:    udagram-api/tslint.json

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=gordon
+export POSTGRES_PASSWORD=s1mPLE-t0N
+export POSTGRES_HOST=database-1.cikwxpuc2tml.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::s3udagord
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
